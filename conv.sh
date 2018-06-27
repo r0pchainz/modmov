734a3ae170e5dbbd8f7564daf3650ad766e1a272
r0pchainz <38400850+r0pchainz@users.noreply.github.com>
2018-06-27 02:26:49 -0500

    append

--- conv.sh
+++ conv.sh
@@ -1,3 +1,5 @@
 find . -iname "*.mov" -exec bash -c 'mv "$0" "${0%\.mov}.mp4"' {} \;
 find . -iname "*.avi" -exec bash -c 'mv "$0" "${0%\.avi}.mp4"' {} \;
 find . -iname "*.flv" -exec bash -c 'mv "$0" "${0%\.flv}.mp4"' {} \;
+find . -iname "*.mkv" -exec bash -c 'mv "$0" "${0%\.mkv}.mp4"' {} \;
+find . -iname "*.wmv" -exec bash -c 'mv "$0" "${0%\.wmv}.mp4"' {} \;
