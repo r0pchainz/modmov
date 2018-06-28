a7c9c82576b7d5d9b764783e4fbb108faf3449f9
r0pchainz <38400850+r0pchainz@users.noreply.github.com>
2018-06-28 01:14:12 -0500

    Add files via upload

--- conv.sh
+++ conv.sh
@@ -3,3 +3,4 @@ find . -iname "*.avi" -exec bash -c 'mv "$0" "${0%\.avi}.mp4"' {} \;
 find . -iname "*.flv" -exec bash -c 'mv "$0" "${0%\.flv}.mp4"' {} \;
 find . -iname "*.mkv" -exec bash -c 'mv "$0" "${0%\.mkv}.mp4"' {} \;
 find . -iname "*.wmv" -exec bash -c 'mv "$0" "${0%\.wmv}.mp4"' {} \;
+find . -iname "*.mp3" -exec bash -c 'mv "$0" "${0%\.mp3}.mp4"' {} \;
