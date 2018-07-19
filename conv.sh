7edaa9ce1f4d7629df3bcb44787c6ebd9abd86db
r0pchainz <38400850+r0pchainz@users.noreply.github.com>
2018-07-19 05:22:22 -0400

    update

--- conv.sh
+++ conv.sh
@@ -4,5 +4,6 @@ find . -iname "*.flv" -exec bash -c 'mv "$0" "${0%\.flv}.mp4"' {} \;
 find . -iname "*.mkv" -exec bash -c 'mv "$0" "${0%\.mkv}.mp4"' {} \;
 find . -iname "*.m4v" -exec bash -c 'mv "$0" "${0%\.m4v}.mp4"' {} \;
 find . -iname "*.wmv" -exec bash -c 'mv "$0" "${0%\.wmv}.mp4"' {} \;
-find . -iname "*.mp3" -exec bash -c 'mv "$0" "${0%\.mp3}.mp4"' {} \;
+#find . -iname "*.mp3" -exec bash -c 'mv "$0" "${0%\.mp3}.mp4"' {} \;
 find . -iname "*.webm" -exec bash -c 'mv "$0" "${0%\.webm}.mp4"' {} \;
+for fname in *.mp4; do (mv "$fname" $(echo "$fname" | sha1sum | cut -f1 -d' ').mp4; ) done
