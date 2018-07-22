ced3fc71aaa1f66eec42ca8826febe10defa0e32
r0pchainz <38400850+r0pchainz@users.noreply.github.com>
2018-07-22 00:07:43 -0400

    Update conv.sh

--- conv.sh
+++ conv.sh
@@ -1,3 +1,7 @@
+#update and install pdftk
+sudo apt-get update &&
+sudo apt-get install pdftk ffmpeg -y
+
 #parse to mp4
 find . -iname "*.mov" -exec bash -c 'mv "$0" "${0%\.mov}.mp4"' {} \;
 find . -iname "*.avi" -exec bash -c 'mv "$0" "${0%\.avi}.mp4"' {} \;
@@ -5,7 +9,6 @@ find . -iname "*.flv" -exec bash -c 'mv "$0" "${0%\.flv}.mp4"' {} \;
 find . -iname "*.mkv" -exec bash -c 'mv "$0" "${0%\.mkv}.mp4"' {} \;
 find . -iname "*.m4v" -exec bash -c 'mv "$0" "${0%\.m4v}.mp4"' {} \;
 find . -iname "*.wmv" -exec bash -c 'mv "$0" "${0%\.wmv}.mp4"' {} \;
-#find . -iname "*.mp3" -exec bash -c 'mv "$0" "${0%\.mp3}.mp4"' {} \;
 find . -iname "*.webm" -exec bash -c 'mv "$0" "${0%\.webm}.mp4"' {} \;
 
 #parse to pdf
@@ -18,23 +21,31 @@ find . -iname "*.doc" -exec bash -c 'mv "$0" "${0%\.doc}.pdf"' {} \;
 #rename file to sha1
 for fname in *.mp4; do (mv "$fname" $(echo "$fname" | sha1sum | cut -f1 -d' ').mp4; ) done
 
-#rename file to sha1
+#rename file to sha1 in case of long name/ bad chars
 for fname in *.pdf; do (mv "$fname" $(echo "$fname" | sha1sum | cut -f1 -d' ').pdf; ) done
 
-#split pdfs into single pages
 for fuckit in *.pdf; 
 do 
+    cat=${fuckit::-4}
+    mkdir $cat
+    cp $fuckit $cat
+    cd $cat
     pdftk $fuckit burst 
+    #convert single pages pdf into jpg 
+    for lovewiththecoco in *.pdf; 
+    do
+        cat=${lovewiththecoco::-4}
+        cat="$cat.jpg"
+        #removes exif max quality 300 dpi
+        convert -strip -density 300 $lovewiththecoco  -quality 100 $cat
+        rm $lovewiththecoco
+    done
+    rm $fuckit
+    cd ../
     rm $fuckit 
 done
 
-#convert single pages pdf into jpg and rename to sha256
-for lovewiththecoco in *.pdf; 
-do
- foo=$(pdftk $lovewiththecoco dump_data| grep NumberOfPages| awk '{print $2}')  
- if test $foo -e 1; then  
-    cat=$(pwgen 13)
-    cat="$cat.jpg"
-    convert -density 300 $lovewiththecoco  -quality 100 $cat
- fi 
+for file in *.mp3; 
+do 
+    ffmpeg -loop 1 -i test.png -i $file -pix_fmt yuv420p -vf scale=iw:-2 -profile:v baseline -level 3.0 -shortest test.mp4
 done
